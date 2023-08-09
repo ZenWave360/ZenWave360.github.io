68fe9b05a01166efb29b595091cac5ae66a1b8af
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2023-08-09 12:19:58 +0200

    build and README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-smooth-doc/README.md
+# ZenWave 360º Website
+
+Website for https://zenwave360.github.io/
+
+# Development
+
+```bash
+npm run --workspace website dev
+```
+
+# Build
+
+```bash
+npm run --workspace website clean
+npm run --workspace website build
+```
+
+# Verify build
+
+```bash
+npm run --workspace website clean
+npm run --workspace website build
+npm run --workspace website serve
+```
--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
         description: "ZenWave 360: DDD and API-First Modeling Tools for Modern Distributed Applications.",
         baseDirectory: path.resolve(__dirname, '../'),
         siteUrl: "https://zenwave360.io/",
-        githubRepositoryURL: "https://github.com/zenwave360/zenwave-sdk",
+        githubRepositoryURL: "https://github.com/zenwave360",
         sections: ['API-First', 'Domain Driven Design', 'Event Driven Architectures', 'ZenWave SDK', 'Enterprise Integration Patterns'],
         navItems: [
           { title: 'Documentation', url: '/docs/' },
