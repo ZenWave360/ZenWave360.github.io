4b9fe0bed5e319df49fd2e6a7c49a9926259ead8
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-03-08 11:00:01 +0100

    adds new post: Spring-Modulith-Events-Spring-Cloud-Stream-Externalizer.mdx

--- smooth-doc/src/components/blog/PostLink.js
+++ smooth-doc/src/components/blog/PostLink.js
@@ -17,7 +17,11 @@ function joinPath(path, file) { return `${path}/${file}`.replaceAll('//', '/') }
 const PostLink = (post) => (
   <article className="post-link" style={postLinkStyle}>
       <Link to={post.slug} style={aStyle}>
-          <img src={joinPath(post.slug, post.image)} alt={post.excerpt} style={{float: 'left', width: '30%', marginRight: '20px'}} />
+          <img 
+            src={post.image?.startsWith('https://') ? post.image : joinPath(post.slug, post.image)} 
+            alt={post.excerpt} 
+            style={{float: 'left', width: '30%', marginRight: '20px'}} 
+          />
           <h2 style={{fontSize: '1.3em', marginBottom: '5px'}}>{post.title || post.slug} / <small style={{fontSize: '0.6em'}}>{post.date}</small></h2>
           <section className="post-link-excerpt" style={{ marginTop: '10px'}}>
               <p>{post.excerpt}</p>
